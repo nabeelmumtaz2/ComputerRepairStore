02ec75d3eb32e644b54218aa9021b99fa561c99d
Nabeel Mumtaz <Nabeel Mumtaz@NABEEL>
2019-02-01 21:22:56 -0500

    Create Controllers

---
+++ ComputerRepair/ComputerRepair/Controllers/AgentsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ComputerRepair;
+
+namespace ComputerRepair.Controllers
+{
+    public class AgentsController : Controller
+    {
+        private CrContainer db = new CrContainer();
+
+        // GET: Agents
+        public ActionResult Index()
+        {
+            var agents = db.Agents.Include(a => a.System);
+            return View(agents.ToList());
+        }
+
+        // GET: Agents/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Agents agents = db.Agents.Find(id);
+            if (agents == null)
+            {
+                return HttpNotFound();
+            }
+            return View(agents);
+        }
+
+        // GET: Agents/Create
+        public ActionResult Create()
+        {
+            ViewBag.SystemsSystem_Id = new SelectList(db.Systems1, "System_Id", "Brands");
+            return View();
+        }
+
+        // POST: Agents/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Agent_Id,First_Name,Last_Name,Employ_Id,Shift_Time,SystemsSystem_Id")] Agents agents)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Agents.Add(agents);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.SystemsSystem_Id = new SelectList(db.Systems1, "System_Id", "Brands", agents.SystemsSystem_Id);
+            return View(agents);
+        }
+
+        // GET: Agents/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Agents agents = db.Agents.Find(id);
+            if (agents == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.SystemsSystem_Id = new SelectList(db.Systems1, "System_Id", "Brands", agents.SystemsSystem_Id);
+            return View(agents);
+        }
+
+        // POST: Agents/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Agent_Id,First_Name,Last_Name,Employ_Id,Shift_Time,SystemsSystem_Id")] Agents agents)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(agents).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.SystemsSystem_Id = new SelectList(db.Systems1, "System_Id", "Brands", agents.SystemsSystem_Id);
+            return View(agents);
+        }
+
+        // GET: Agents/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Agents agents = db.Agents.Find(id);
+            if (agents == null)
+            {
+                return HttpNotFound();
+            }
+            return View(agents);
+        }
+
+        // POST: Agents/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Agents agents = db.Agents.Find(id);
+            db.Agents.Remove(agents);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ComputerRepair/ComputerRepair/Controllers/SystemsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ComputerRepair;
+
+namespace ComputerRepair.Controllers
+{
+    public class SystemsController : Controller
+    {
+        private CrContainer db = new CrContainer();
+
+        // GET: Systems
+        public ActionResult Index()
+        {
+            var systems1 = db.Systems1.Include(s => s.Customer);
+            return View(systems1.ToList());
+        }
+
+        // GET: Systems/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Systems systems = db.Systems1.Find(id);
+            if (systems == null)
+            {
+                return HttpNotFound();
+            }
+            return View(systems);
+        }
+
+        // GET: Systems/Create
+        public ActionResult Create()
+        {
+            ViewBag.CustomersClient_Id = new SelectList(db.Customers, "Client_Id", "First_Name");
+            return View();
+        }
+
+        // POST: Systems/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "System_Id,Brands,Note,Drop_Off_Date,Pick_Up_Date,Quote,CustomersClient_Id")] Systems systems)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Systems1.Add(systems);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CustomersClient_Id = new SelectList(db.Customers, "Client_Id", "First_Name", systems.CustomersClient_Id);
+            return View(systems);
+        }
+
+        // GET: Systems/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Systems systems = db.Systems1.Find(id);
+            if (systems == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CustomersClient_Id = new SelectList(db.Customers, "Client_Id", "First_Name", systems.CustomersClient_Id);
+            return View(systems);
+        }
+
+        // POST: Systems/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "System_Id,Brands,Note,Drop_Off_Date,Pick_Up_Date,Quote,CustomersClient_Id")] Systems systems)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(systems).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CustomersClient_Id = new SelectList(db.Customers, "Client_Id", "First_Name", systems.CustomersClient_Id);
+            return View(systems);
+        }
+
+        // GET: Systems/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Systems systems = db.Systems1.Find(id);
+            if (systems == null)
+            {
+                return HttpNotFound();
+            }
+            return View(systems);
+        }
+
+        // POST: Systems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Systems systems = db.Systems1.Find(id);
+            db.Systems1.Remove(systems);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
